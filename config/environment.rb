76afde0ed0e763490d660961ae97137728999870
robwhelan <whelan@gmail.com>
2012-10-26 11:27:44 -0400

    fixed mail settings

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 DemandoAuction::Application.initialize!
+
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => 'app8701821@heroku.com',
+  :password       => 'iye5qabm',
+  :domain         => 'www.deman.do',
+  :enable_starttls_auto => true
+}
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -50,6 +50,7 @@ DemandoAuction::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
+	config.action_mailer.default_url_options = { :host => 'www.deman.do' }
 
   # Enable threaded mode
   # config.threadsafe!
