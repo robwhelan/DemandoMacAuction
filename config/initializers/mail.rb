9c0819c8a794bbcfeb2ed3d03dcc02ab26e2e30e
robwhelan <whelan@gmail.com>
2012-10-26 11:08:40 -0400

    added mail capabilities through sendgrid; testing now

---
+++ config/initializers/mail.rb
@@ -0,0 +1,10 @@
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => 'app8701821@heroku.com',
+  :password       => 'iye5qabm',
+  :domain         => 'www.deman.do'
+  :enable_starttls_auto => true
+}
+ActionMailer::Base.delivery_method = :smtp
